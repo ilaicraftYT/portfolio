594b9203f9e2600c640b671cfc42d39aff6bf183
ilaicraftYT <rubencitoperez569@gmail.com>
2024-03-22 17:38:39 -0600

    💄 Fix extra margin in blog notes

--- src/lib/components.js
+++ src/lib/components.js
@@ -2,10 +2,10 @@ import { h } from "hastscript"
 import { visit, SKIP } from "unist-util-visit"
 import { isElement } from "hast-util-is-element"
 
-const Note = (props, children) => h(".note", [h("p.text-sapphire", "Note"), ...children]) 
-const Tip = (props, children) => h(".tip", [h("p.text-green", "Tip"), ...children])
-const Important = (props, children) => h(".important", [h("p.text-mauve", "Important"), ...children])
-const Warning = (props, children) => h(".warning", [h("p.text-yellow", "Warning"), ...children])
-const Caution = (props, children) => h(".caution", [h("p.text-red", "Caution"), ...children])
+const Note = (props, children) => h(".note", [h("p.text-sapphire.my-2", "Note"), ...children]) 
+const Tip = (props, children) => h(".tip", [h("p.text-green.my-2", "Tip"), ...children])
+const Important = (props, children) => h(".important", [h("p.text-mauve.my-2", "Important"), ...children])
+const Warning = (props, children) => h(".warning", [h("p.text-yellow.my-2", "Warning"), ...children])
+const Caution = (props, children) => h(".caution", [h("p.text-red.my-2", "Caution"), ...children])
 
 export { Note, Tip, Important, Warning, Caution }
