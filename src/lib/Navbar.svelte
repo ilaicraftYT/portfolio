7eeec3124b4327e4c18ac9503b272caab72a05dc
ilaicraftYT <rubencitoperez569@gmail.com>
2024-03-22 17:57:34 -0600

    ✨ Replace toggle with actual dark/light button

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { isDark } from '$lib/store';
+	import NightIcon from '@tabler/icons-svelte/dist/svelte/icons/IconMoonStars.svelte';
+	import SunIcon from '@tabler/icons-svelte/dist/svelte/icons/IconSun.svelte';
 
 	function toggleDarkMode() {
 		isDark.update((v) => !v);
@@ -8,10 +10,11 @@
 
 <nav class="bg-surface0 text-text rounded-xl flex justify-between p-3">
 	<a class="font-bold cursor-pointer" href="/">Soy<span class="text-green">Ilai</span></a>
-	<label class="relative inline-flex items-center cursor-pointer">
-		<input type="checkbox" on:change={toggleDarkMode} checked={$isDark} class="sr-only peer" />
-		<div
-			class="w-11 h-6 bg-surface1 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content_[''] after:absolute after:top-[2px] after:left_[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green"
-		/>
-	</label>
+	<button class="text-text hover:text-green" on:click={toggleDarkMode}>
+		{#if $isDark}
+			<SunIcon />
+		{:else}
+			<NightIcon />
+		{/if}
+	</button>
 </nav>
--- src/lib/components/ThemeCard.svelte
+++ src/lib/components/ThemeCard.svelte
@@ -2,6 +2,8 @@
 	import { isDark } from '$lib/store';
 
 	import GridCard from '$lib/components/GridCard.svelte';
+	import NightIcon from "@tabler/icons-svelte/dist/svelte/icons/IconMoonStars.svelte"
+	import SunIcon from "@tabler/icons-svelte/dist/svelte/icons/IconSun.svelte"
 
 	function toggleDarkMode() {
 		isDark.update((v) => !v);
@@ -9,10 +11,11 @@
 </script>
 
 <GridCard color="bg-surface0" extraClasses="flex justify-center items-center">
-	<label class="relative inline-flex items-center cursor-pointer">
-		<input type="checkbox" on:change={toggleDarkMode} checked={$isDark} class="sr-only peer" />
-		<div
-			class="w-11 h-6 bg-surface1 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content_[''] after:absolute after:top-[2px] after:left_[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green"
-		/>
-	</label>
+	<button class="text-text hover:text-green" on:click={toggleDarkMode}>
+		{#if $isDark}
+			<SunIcon size={48} />
+		{:else}
+			<NightIcon size={48} />
+		{/if}
+	</button>
 </GridCard>
