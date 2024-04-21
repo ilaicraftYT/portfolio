573d9088d8dc8b44cc0e7ee7e8debb4c7204d87c
ilacraftYT <rubencitoperez569@gmail.com>
2024-04-21 15:56:15 -0600

    ✨ Automatic date-related cards update

--- src/lib/components/AgeCard.svelte
+++ src/lib/components/AgeCard.svelte
@@ -1,13 +1,14 @@
 <script>
 	import GridCard from '$lib/components/GridCard.svelte';
-</script>
 
+	const age = Math.abs(new Date(Date.now() - new Date('2010-10-13')).getUTCFullYear() - 1970);
+</script>
 
 <GridCard color="bg-red" extraClasses="">
 	<div>
 		<div class="flex flex-col items-center justify-center">
 			<h3 class="text-1xl text-light">Age</h3>
-			<p class="text-3xl font-black">13</p>
+			<p class="text-3xl font-black">{age}</p>
 			<p class="font-medium">years old</p>
 		</div>
 	</div>
--- src/lib/components/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard.svelte
@@ -1,11 +1,13 @@
 <script>
 	import GridCard from '$lib/components/GridCard.svelte';
+
+	const started = Math.abs(new Date(Date.now() - new Date('2019')).getUTCFullYear() - 1970);
 </script>
 
 <GridCard color="bg-sky" extraClasses="flex justify-center items-center overflow-hidden p-4">
 	<div>
 		<div class="flex flex-col justify-center items-center">
-			<p class="text-4xl font-black">+3</p>
+			<p class="text-4xl font-black">+{started}</p>
 			<p class="text-1xl font-medium">years of</p>
 			<p class="text-1xl font-medium leading-4">experience</p>
 		</div>
