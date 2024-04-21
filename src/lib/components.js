028ab1a0ff8480367f790da3f0d8e0acdeaad25c
ilacraftYT <rubencitoperez569@gmail.com>
2024-04-21 15:48:12 -0600

    🔥 Trash

--- src/lib/components.js
+++ src/lib/components.js
@@ -84,11 +84,4 @@ const Caution = (props, children) =>
 		]),
 		...children
 	]);
-/* const Tip = (props, children) => h('.tip', [h('p.my-2',), ...children]);
-const Important = (props, children) =>
-  h('.important', [h('p.text-mauve.my-2', 'Important'), ...children]);
-const Warning = (props, children) =>
-  h('.warning', [h('p.text-yellow.my-2', 'Warning'), ...children]);
-const Caution = (props, children) => h('.caution', [h('p.text-red.my-2', 'Caution'), ...children]); */
-
 export { Note, Tip, Important, Warning, Caution };
