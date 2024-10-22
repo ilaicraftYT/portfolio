4a0629688c03dac067db02005e3e5274713d82bc
ilaicraftYT <soyilai@proton.me>
2024-10-21 18:54:12 -0600

    feat (fav songs): add more

--- src/lib/components/SpotifyCard.svelte
+++ src/lib/components/SpotifyCard.svelte
@@ -3,9 +3,22 @@
 	import IconBrandSpotify from '@tabler/icons-svelte/IconBrandSpotify.svelte';
 </script>
 
-<GridCard bento={true} color="bg-[#1db954]" extraClasses="dark:text-text p-4">
+<GridCard bento={true} color="bg-[#1db954]" extraClasses="dark:text-text p-4 overflow-hidden">
 	<div class="inline-flex">
-		<IconBrandSpotify /> <span class="pl-1 font-medium">Favorite song</span>
+		<IconBrandSpotify /> <span class="pl-1 font-medium">Favorite songs</span>
 	</div>
-	<p><b>Llorar</b> - Los Socios Del Ritmo</p>
+	<div class="grid text-xs sm:grid-cols-2 sm:text-base/6">
+		<p>
+			<span><b>Llorar</b> - Los Socios Del Ritmo</span>
+		</p>
+		<p>
+			<span><b>El Afilador</b> - Grupo Carabo</span>
+		</p>
+		<p>
+			<span><b>Perfecta</b> - Miranda!</span>
+		</p>
+		<p>
+			<span><b>Yo Quisiera</b> - Reik</span>
+		</p>
+	</div>
 </GridCard>
