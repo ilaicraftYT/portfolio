a84ad7af0047c7ff51e0f72dc1f5393f19d7fa46
Ilai <ilai@chimoteam.eu.org>
2026-03-20 15:36:56 -0600

    update github

--- src/lib/components/FirstSocialGroup.svelte
+++ src/lib/components/FirstSocialGroup.svelte
@@ -4,7 +4,7 @@
 	import IconBrandLinkedin from '@tabler/icons-svelte/icons/brand-linkedin';
 </script>
 
-<GridCard color="bg-[#333]" href="https://github.com/ilaicraftYT">
+<GridCard color="bg-[#333]" href="https://github.com/soyilai">
 	<IconBrandGithub class="dark:text-text" size="3rem" />
 </GridCard>
 <GridCard color="bg-[#0a66c2]" href="https://linkedin.com/in/soyilai">
